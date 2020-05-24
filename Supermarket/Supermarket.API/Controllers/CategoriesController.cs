c015538944afca5cb9fffb3b8e9de37938bc439f
Ayoola Akindolani <aakindolani@eminenttechnology.com>
2020-05-25 00:32:01 +0100

    Add project

---
+++ Supermarket/Supermarket.API/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            return categories;
+        }
+    }
+}
